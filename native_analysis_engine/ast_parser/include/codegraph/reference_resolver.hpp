c558d2e7c170462ba511d23e64bff2d72f3a0a20
Andezion <vdoronchenkov@gmail.com>
2026-08-19 21:57:13 +0200

    add: added new struct

--- native_analysis_engine/ast_parser/include/codegraph/reference_resolver.hpp
+++ native_analysis_engine/ast_parser/include/codegraph/reference_resolver.hpp
@@ -13,6 +13,7 @@ void resolve_pending_references(Graph& graph, IdGenerator& id_gen,
                                  const std::vector<PendingFieldAccess>& pending_reads,
                                  const std::vector<PendingFieldAccess>& pending_writes,
                                  const std::vector<PendingImport>& pending_imports,
+                                 const std::vector<PendingContains>& pending_contains,
                                  const std::unordered_map<std::string, std::string>& usr_to_node_id,
                                  const std::unordered_map<std::string, std::string>& path_to_file_node_id);
 
